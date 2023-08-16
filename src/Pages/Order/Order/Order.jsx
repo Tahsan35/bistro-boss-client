c9728847d3535634f24e50ee6e3d7df7b44c520f
Tahsan <tahsanknmas14@gmail.com>
2023-08-16 21:06:20 +0600

    added order card

---
+++ src/Pages/Order/Order/Order.jsx
@@ -0,0 +1,36 @@
+import {Tab, Tabs, TabList, TabPanel} from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import {useState} from "react";
+import useMenu from "../../../hooks/useMenu";
+
+const Order = () => {
+  const categories = ["salad", "pizza", "soup", "dessert", "drinks"];
+  const {category} = useParams();
+  const initialIndex = categories.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialIndex);
+  const [menu] = useMenu();
+
+  const desserts = menu.filter(item => item.category === "dessert");
+  const soup = menu.filter(item => item.category === "soup");
+  const salad = menu.filter(item => item.category === "salad");
+  const pizza = menu.filter(item => item.category === "pizza");
+  const drinks = menu.filter(item => item.category === "drinks");
+
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>Title 1</Tab>
+        <Tab>Title 2</Tab>
+      </TabList>
+
+      <TabPanel>
+        <h2>Any content 1</h2>
+      </TabPanel>
+      <TabPanel>
+        <h2>Any content 2</h2>
+      </TabPanel>
+    </Tabs>
+  );
+};
+
+export default Order;
